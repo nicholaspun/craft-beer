5d58795a00340c2579268fe869a8e54a5aae4d6f
Nicholas Pun <nicholaspun99@gmail.com>
2018-11-04 18:22:01 -0500

    write output

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ extern crate serde;
 extern crate serde_json;
 
 use std::io;
+use std::fs;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct BeerEntry {
     #[serde(rename = "Date")] date: String,
     #[serde(rename = "Brewery")] brewery: String,
@@ -18,10 +19,18 @@ struct BeerEntry {
 }
 
 fn main() {
+    // Read from CSV
     let mut rdr = csv::Reader::from_reader(io::stdin());
     let mut beer_results = Vec::new();
     for result in rdr.deserialize() {
         let record: BeerEntry = result.expect("Can't parse result");
         beer_results.push(record)
     }
+
+    // Convert vector to JSON string
+    let serialized = serde_json::to_string(&beer_results).unwrap();
+
+    // Write to file
+    fs::File::create("beer.json").expect("Unable to create file");
+    fs::write("beer.json", serialized).expect("Unable to write file");
 }
